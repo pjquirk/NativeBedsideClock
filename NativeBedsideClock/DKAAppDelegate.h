39fd244b41f301ab8bd0c5b4b6781f249faf35ee
Dan Albertson <dkalbertson@gmail.com>
2013-12-11 21:48:04 -0500

    Initial Commit

---
+++ NativeBedsideClock/DKAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DKAAppDelegate.h
+//  NativeBedsideClock
+//
+//  Created by Daniel Albertson on 12/11/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
